729fded49dedf4acc39646b1fb8f106c6d27d194
Técnico em Informática <24121742018.1@senacrjedu.local>
2019-09-02 21:43:39 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNavComponent } from './pages/page-nav/page-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNavComponent
   ],
   imports: [
     BrowserModule,
